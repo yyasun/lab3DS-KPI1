28f60a0c6cbccc4e0442c7fed71f56decb768fc8
Yaroslav <pablo1231435@gmail.com>
2020-06-03 22:36:33 +0300

    dict dependent on arr.length

--- Dictionary.cs
+++ Dictionary.cs
@@ -1,36 +1,97 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
+using System.Threading;
 
 namespace Lab3DS
 {
     public class Dictionary
     {
-        List<string>[] arr;
+        LinkedList<KeyValuePair<string, string>>[] arr;
+        long count;
+        const int A = 5;
+        const int startAmount = 8;
+        public Dictionary()
+        {
+            arr = new LinkedList<KeyValuePair<string, string>>[startAmount];
+        }
         public string this[string key]
         {
-            get 
+            get
             {
+                var i = Hash(PreHash(key), arr.Length);
 
-                return null;
+                return arr[i].FirstOrDefault(x => x.Key == key).Value;
             }
             set
             {
-                //exists and dont exist;
+                count++;
+                if (count > arr.Length * A)
+                {
+                    Rehash(arr.Length * 2);
+                }
+                Add(key, value, arr);
             }
         }
-        private long Hash(long hash)
+        public void Remove(string key)
         {
-            return hash %
+            count--;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                KeyValuePair<string, string> a= new KeyValuePair<string, string>();
+                try
+                {
+                    a = arr[i].First(x => x.Key == key);                  
+                }
+                catch
+                {
+                }
+                arr[i].Remove(a);
+                break;
+            }
+            if (count < (arr.Length * A) / 4)
+            {
+                Rehash(arr.Length / 4);
+            }
+
+        }
+        private void Add(string key, string value, LinkedList<KeyValuePair<string, string>>[] Arr)
+        {
+            var h = Hash(PreHash(key), Arr.Length);
+            if (Arr[h] == null)
+                Arr[h] = new LinkedList<KeyValuePair<string, string>>();
+            Arr[h].AddFirst(new KeyValuePair<string, string>(key, value));
+        }
+        private void Rehash(int newLen)
+        {
+            var tmpArr = new LinkedList<KeyValuePair<string, string>>[newLen];
+
+            foreach (var lst in arr)
+            {
+                if(lst!=null)
+                foreach (var kp in lst)
+                {
+                    Add(kp.Key, kp.Value, tmpArr);
+                }
+            }
+            arr = tmpArr;
+            Console.WriteLine("test");
+        }
+        private long Hash(long hash, int length)
+        {
+            return hash % length;
         }
         private long PreHash(string str)
         {
-            long hash = 5381;            
-            foreach(var c in str)
+            long hash = 5381;
+            foreach (var c in str)
                 hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
             return hash;
         }
-        
+
     }
-    
+
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace Lab3DS
 {
@@ -6,6 +7,24 @@ namespace Lab3DS
     {
         static void Main(string[] args)
         {
+            var a = DateTime.Now;
+
+            Dictionary dict = new Dictionary();
+            dict["meme13212"] = "123123123boiiiiiii";
+            dict["meme"] = "boiiiiiii";
+            Console.WriteLine(dict["meme"]);
+            dict["asdsdasd"] = "lorem ipsum";
+            dict["meme13212"] = "123123123boiiiiiii";
+            dict["meme"] = "dank bro";
+            for (int i = 0; i < 1000000; i++)
+            {
+                dict[i.ToString()] = "i" + 123 + i.ToString();
+            }
+            var b = DateTime.Now;            
+            Console.WriteLine(dict["meme"]);
+            Console.WriteLine(DateTime.Now - b);
+            Console.WriteLine();
+            Console.WriteLine(DateTime.Now - a);
             
         }
     }
