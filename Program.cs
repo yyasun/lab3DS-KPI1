673af81bd207b58b236c3cf366b94a02a6acce15
Yaroslav <pablo1231435@gmail.com>
2020-06-03 13:36:38 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab3DS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
