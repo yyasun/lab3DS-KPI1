8681a4c1331425d53293fd0f1b1079042391bbb1
Yaroslav <pablo1231435@gmail.com>
2020-06-03 13:43:59 +0300

    created dict

---
+++ Dictionary.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab3DS
+{
+    public class Dictionary
+    {
+        List<string>[] arr;
+        public string this[string key]
+        {
+            get 
+            {
+
+                return null;
+            }
+            set
+            {
+                //exists and dont exist;
+            }
+        }
+        private long Hash(long hash)
+        {
+            return hash %
+        }
+        private long PreHash(string str)
+        {
+            long hash = 5381;            
+            foreach(var c in str)
+                hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+            return hash;
+        }
+        
+    }
+    
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Lab3DS
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
